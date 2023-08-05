e6d8f399ed59ae1fb96ea922607ddbf04dc864b8
Askelo <kulik.working@gmail.com>
2023-08-05 11:49:28 +0300

    -fix: ffffu..

--- common/errors.js
+++ common/errors.js
@@ -1,7 +1,7 @@
 module.exports.handleErrors = (errorName) => {
   const listErrors = {
     ValidationError: { code: 400, title: 'Переданы некорректные данные' },
-    CastError: { code: 400, title: 'Некорректный ID' },
+    CastError: { code: 404, title: 'Некорректный ID' },
     notFound: { code: 400, title: 'Данные по указанному ID не найдены' },
     empty: { code: 400, title: 'Переданы некорректные данные' },
     notOwner: { code: 400, title: 'Нет прав для совершения действия' },
