f539508ef7f7bdea6b16f42b3efba947d629f794
Askelo <kulik.working@gmail.com>
2023-08-12 23:04:31 +0300

    -fix: celebrate fix

--- middlewares/handlerError.js
+++ middlewares/handlerError.js
@@ -1,9 +1,9 @@
-import { errors } from 'celebrate';
+import { isCelebrateError } from 'celebrate';
 
 // eslint-disable-next-line no-unused-vars
 export default (err, req, res, next) => {
   const { message = 'На сервере произошла ошибка' } = err;
   let { statusCode = 500 } = err;
-  if (errors()) statusCode = 400;
+  if (isCelebrateError(err)) statusCode = 400;
   res.status(statusCode).send({ message });
 };
