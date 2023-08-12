902f727416e42439072f06e8ebacad6fa1991c85
Askelo <kulik.working@gmail.com>
2023-08-12 22:32:44 +0300

    -fix: beatchh!!

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,16 @@ import { Router } from 'express';
 import {
   getUsers, getUserByID, getUserMe, updateProfile, updateAvatar,
 } from '../controllers/users.js';
-import Validator from '../common/validator.js';
+// import Validator from '../common/validator.js';
 
 const router = Router();
-const { updUserInfoValidator, updUserAvatarValidator } = Validator();
+// const { updUserInfoValidator, updUserAvatarValidator } = Validator();
 
 router.get('/', getUsers);
 router.get('/me', getUserMe);
 router.get('/:id', getUserByID);
 
-router.patch('/me', updUserInfoValidator, updateProfile);
-router.patch('/me/avatar', updUserAvatarValidator, updateAvatar);
+router.patch('/me', updateProfile);
+router.patch('/me/avatar', updateAvatar);
 
 export default router;
